c0662a128823abc6d6a8aefbe41b5137b6f49b9e
Pavan Chilukuri <pavan.chilukuri@dhcs.ca.gov>
2020-10-17 16:29:44 -0700

    Add Expire component

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders react app', () => {
+  const e = render(<App />);
+  expect(e).toBeTruthy();
 });
